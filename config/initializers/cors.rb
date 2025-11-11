52fc6e37d755cfd75fc33e8ff8a2d8b9e9d17561
Matthew Hart <mhart1000@users.noreply.github.com>
2025-11-11 15:27:18 -0800

    configure cors & ports

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -14,3 +14,14 @@
 #       methods: [:get, :post, :put, :patch, :delete, :options, :head]
 #   end
 # end
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins 'http://localhost:9100'
+    resource '*',
+             headers: :any,
+             methods: %i[get post put patch delete options head],
+             expose: %w[Authorization]
+  end
+end
+
