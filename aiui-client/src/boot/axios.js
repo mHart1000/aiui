9bec1dc5d3e7b5583e205d0c20cdd2b21bf392aa
Matthew Hart <mhart1000@users.noreply.github.com>
2025-11-25 00:32:40 -0800

    expired token redirect

--- aiui-client/src/boot/axios.js
+++ aiui-client/src/boot/axios.js
@@ -14,4 +14,15 @@ api.interceptors.request.use(config => {
   return config
 })
 
+api.interceptors.response.use(
+  response => response,
+  error => {
+    if (error.response && error.response.status === 401) {
+      localStorage.removeItem('jwt')
+      window.location.href = '/#/login'
+    }
+    return Promise.reject(error)
+  }
+)
+
 export { api }
