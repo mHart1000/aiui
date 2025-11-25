dd4b35c31f17047c0fca6ee4f157e8080aa97279
Matthew Hart <mhart1000@users.noreply.github.com>
2025-11-24 22:27:21 -0800

    signup & login work

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,17 +1,29 @@
 module Api
   class SessionsController < Devise::SessionsController
     respond_to :json
+    skip_before_action :verify_signed_out_user, only: :destroy
 
     def create
-      user = warden.authenticate!(auth_options)
-      sign_in(user, store: false)
+      creds = params[:user]
 
-      token = request.env['warden-jwt_auth.token']
-      render json: { user: user, token: token }, status: :ok
+      email = creds[:email].to_s
+      password = creds[:password].to_s
+
+      user = User.find_for_database_authentication(email: email)
+
+      if user&.valid_password?(password)
+        sign_in(user, store: false)
+
+        token = request.env['warden-jwt_auth.token']
+
+        render json: { user: user, token: token }, status: :ok
+      else
+        render json: { error: 'Invalid email or password' }, status: :unauthorized
+      end
     end
 
     def destroy
-      render json: { message: "Logged out" }, status: :ok
+      render json: { message: 'Logged out' }, status: :ok
     end
   end
 end
